31317f6009c3632b85c7aee9624391da76473d1b
Joey Smith <jsmith@webinertia.net>
2022-04-04 23:38:30 -0500

    Adding TableGateway, AbstractModel, ModelTrait Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

---
+++ src/AbstractModel.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webinertia\ModelManager;
+
+use Laminas\Config\Config;
+use Laminas\Db\ResultSet\Exception\InvalidArgumentException;
+use Laminas\Db\ResultSet\ResultSet;
+use Laminas\Db\Sql\Sql;
+use Laminas\EventManager\EventManager;
+use Laminas\Log\Logger;
+use Laminas\Permissions\Acl\ProprietaryInterface;
+use Laminas\Permissions\Acl\Resource\ResourceInterface;
+use Laminas\Permissions\Acl\Role\RoleInterface;
+use Laminas\Stdlib\ArrayObject;
+use Webinertia\ModelManager\ModelInterface;
+use Webinertia\ModelManager\ModelManager;
+use Webinertia\ModelManager\TableGateway\TableGateway;
+
+abstract class AbstractModel extends ArrayObject implements
+    ResourceInterface,
+    ProprietaryInterface,
+    RoleInterface,
+    ModelInterface
+{
+    /** @var Config $config */
+    protected $config;
+    /** @var TableGateway $db; */
+    protected $db;
+    /** @var Logger $logger */
+    protected $logger;
+    /**
+     * String column name for the ownerId (usually points at the Users table id column)
+     *
+     * @var string $ownerIdColumn
+     * */
+    protected $ownerIdColumn;
+    /** @var ModelManager $modelManager */
+    protected $modelManager;
+    /**
+     * @param string $table
+     * @return void
+     * @throws InvalidArgumentException
+     * @throws ExceptionInvalidArgumentException
+     */
+    public function __construct($table, ModelManager $modelManager, EventManager $eventManager, Config $config, ?Logger $logger = null)
+    {
+        $this->modelManager = $modelManager;
+        $resultSetPrototype = new ResultSet();
+        $resultSetPrototype->setArrayObjectPrototype($this);
+        $this->db     = new TableGateway($table, $eventManager, $resultSetPrototype);
+        $this->config = $config;
+        parent::__construct([], ArrayObject::ARRAY_AS_PROPS);
+    }
+
+    public function getTablegateway(): TableGateway
+    {
+        return $this->db;
+    }
+
+    public function getRoleId(): string
+    {
+        if ($this->offsetExists('role')) {
+            return $this->offsetGet('role');
+        }
+        return null;
+    }
+
+    public function getOwnerId(): int
+    {
+        if (! empty($this->ownerIdColumn) && $this->offsetExists($this->ownerIdColumn)) {
+            return $this->offsetGet($this->ownerIdColumn);
+        } elseif ($this->offsetExists('userId')) {
+            return $this->offsetGet('userId');
+        } elseif ($this->offsetExists('user_id')) {
+            return $this->offsetGet('user_id');
+        }
+    }
+
+    public function getResourceId(): string
+    {
+        return $this->db->getTable();
+    }
+
+    public function toArray(): array
+    {
+        return $this->getArrayCopy();
+    }
+
+    public function getSql(): Sql
+    {
+        return $this->db->getSql();
+    }
+
+    public function getResultSetPrototype(): ResultSet
+    {
+        return $this->db->getResultSetPrototype();
+    }
+}
--- src/ModelManager.php
+++ src/ModelManager.php
@@ -8,10 +8,15 @@ use Laminas\ServiceManager\AbstractPluginManager;
 use Laminas\ServiceManager\Exception\InvalidServiceException;
 use Webinertia\ModelManager\ModelInterface;
 
+use function get_class;
+use function gettype;
+use function is_object;
+use function sprintf;
+
 /**
- * laminas-servicemanager v3-compatible plugin manager implementation for form elements.
+ * laminas-servicemanager v3-compatible plugin manager implementation for Models.
  *
- * Enforces that elements retrieved are instances of ElementInterface.
+ * Enforces that elements retrieved are instances of ModelInterface.
  */
 class ModelManager extends AbstractPluginManager
 {
@@ -54,14 +59,14 @@ class ModelManager extends AbstractPluginManager
      */
     public function validate($plugin)
     {
-        parent::validate($plugin);
-        // if (! $plugin instanceof $this->instanceOf) {
-        //     throw new InvalidServiceException(sprintf(
-        //         '%s can only create instances of %s; %s is invalid',
-        //         get_class($this),
-        //         $this->instanceOf,
-        //         is_object($plugin) ? get_class($plugin) : gettype($plugin)
-        //     ));
-        // }
+        //parent::validate($plugin);
+        if (! $plugin instanceof $this->instanceOf) {
+            throw new InvalidServiceException(sprintf(
+                '%s can only create instances of %s; %s is invalid',
+                get_class($this),
+                $this->instanceOf,
+                is_object($plugin) ? get_class($plugin) : gettype($plugin)
+            ));
+        }
     }
 }
