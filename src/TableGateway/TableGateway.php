8001e0a8fa2bea52738aecc87835f87f0e3c2d6d
Joey Smith <jsmith@webinertia.net>
2022-04-20 22:22:11 -0500

    committing chnages to tablegateway class Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/TableGateway/TableGateway.php
+++ src/TableGateway/TableGateway.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Webinertia\ModelManager\TableGateway;
 
-//use Application\Listener\TableGatewayListener;
 use Application\Model\AbstractModel;
 use Laminas\Db\ResultSet\ResultSet;
 use Laminas\Db\TableGateway\AbstractTableGateway;
@@ -12,7 +11,6 @@ use Laminas\Db\TableGateway\Exception\RuntimeException;
 use Laminas\Db\TableGateway\Feature\EventFeature;
 use Laminas\Db\TableGateway\Feature\FeatureSet;
 use Laminas\Db\TableGateway\Feature\GlobalAdapterFeature;
-use Laminas\Db\TableGateway\Feature\MetadataFeature;
 use Laminas\EventManager\EventManager;
 
 class TableGateway extends AbstractTableGateway
@@ -38,11 +36,8 @@ class TableGateway extends AbstractTableGateway
         $this->featureSet->setTableGateway($this);
         // Add the desired features
         $this->featureSet->addFeature(new GlobalAdapterFeature());
-        $this->featureSet->addFeature(new MetadataFeature());
         // pass an instance of the EventManager
         $eventFeature = new EventFeature($eventManager);
-        //$listener     = new TableGatewayListener();
-        //$listener->attach($eventFeature->getEventManager());
         $this->featureSet->addFeature($eventFeature);
         $this->resultSetPrototype = $resultSetPrototype ?? new ResultSet();
         // inititalize this instance
